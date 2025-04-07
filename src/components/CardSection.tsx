07c2a3f17a1a74c4aacd0c0a6b9641a4c395e50e
Haroun <guessous.haroun12@gmail.com>
2025-04-07 14:33:37 -0400

    sqush

--- src/components/CardSection.tsx
+++ src/components/CardSection.tsx
@@ -1,14 +1,8 @@
 "use client";
 
 import React from 'react';
-import TimerCard from './TimerCard';
-import WeatherCard from './WeatherCard';
 import BooksCard from './BooksCard';
-import SpotifyCard from './SpotifyCard';
-import PhotoCard from './PhotoCard';
 import MontrealMapCard from './MontrealMapCard';
-import SocialCard from './SocialCard';
-import DocsCard from './DocsCard';
 import ExperienceCard from './ExperienceCard';
 import ProjectsCard from './ProjectsCard';
 
@@ -17,16 +11,10 @@ const CardSection: React.FC = () => {
     <section className="px-4 py-5">
       <div className="container mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-          <TimerCard />
-          <WeatherCard />
-          <DocsCard />
-          <MontrealMapCard />
-          <SocialCard />
           <ExperienceCard />
-          <ProjectsCard />
+          <MontrealMapCard />
           <BooksCard />
-          <PhotoCard />
-          <SpotifyCard />
+          <ProjectsCard />
         </div>
       </div>
     </section>
